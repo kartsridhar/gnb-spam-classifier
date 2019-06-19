a63404adee46b0fc402f2c481f04eaa9f138c045
kartsridhar <contactkarthik34@gmail.com>
2019-06-19 23:19:41 +0100

    able to render HTML template from hello-world

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # flask = Framework
 # Flask = Python Class -> used to create instances of web applications
 
 app = Flask(__name__)           # creates the instance of the class
 
-@app.route('/hello')                 # function that is mapped to the URL
+@app.route('/')                 # function that is mapped to the URL
 def home():
-    return "Hello World!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)             # debug = true to print out possible python errors
