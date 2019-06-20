fafa16e1689b21ace409fff1d11bcca5038c2f78
kartsridhar <contactkarthik34@gmail.com>
2019-06-20 12:27:35 +0100

    training done, making changes

--- spam-classifier/classifier.py
+++ spam-classifier/classifier.py
@@ -7,10 +7,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 df = pd.read_csv('data/training-data.csv', encoding="latin-1")
 
-vectorizer = CountVectorizer()
-df['message'] = vectorizer.fit_transform(df['message'])
+X = df['message'].values
+y = df['class'].values
 
-train_set, test_set, train_classes, test_classes = train_test_split(df['message'], df['class'], test_size=0.3, random_state=42)
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(X)
+
+train_set, test_set, train_classes, test_classes = train_test_split(X, y, test_size=0.3, random_state=42)
+
+classifier = MultinomialNB()
+classifier.fit(train_set, train_classes)
+classifier.score(test_set, test_classes)
+
+prediction = classifier.predict(test_set)
+print(classification_report(test_classes, prediction))
